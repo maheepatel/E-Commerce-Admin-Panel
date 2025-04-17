9ddf9fd83c3132ab5929170a5f2d36c752029e45
maheepatel <mahendarptl9@gmail.com>
2025-04-17 23:23:34 +0530

    ADD: Admin auth

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+
+// Components
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+// Pages
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base ">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
